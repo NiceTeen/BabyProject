c8e3e0cb3c94ebf780e80c1782be24baa1151099
NiceTeen <15058455670@163.com>
2025-03-16 22:13:24 +0800

    增加提取货号的功能

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ class ConfigSave:
             control.setText(value)
 
 
-class MainFrame(QWidget):
+class MainFrame(QTabWidget):
     """界面类"""
 
     signal_hint_error = QtCore.pyqtSignal(str)
@@ -219,43 +219,92 @@ class MainFrame(QWidget):
         self.setWindowIcon(QtGui.QIcon("ico.ico"))
         self.resize(600, 400)
 
-        vbox = QVBoxLayout(self)
-        self.setLayout(vbox)
+        # 拆分功能
+        self.widget1 = QWidget(self)
 
-        hbox_start = QHBoxLayout(self)
-        self.start_button = QPushButton("开始处理", self)
+        vbox = QVBoxLayout(self.widget1)
+        self.widget1.setLayout(vbox)
+
+        hbox_start = QHBoxLayout(self.widget1)
+        self.start_button = QPushButton("开始处理", self.widget1)
         self.start_button.clicked.connect(self.start_handle)
         hbox_start.addWidget(self.start_button)
         vbox.addLayout(hbox_start)
 
-        hbox_path = QHBoxLayout(self)
-        self.file_path_ctrl = QLineEdit(self)
+        hbox_path = QHBoxLayout(self.widget1)
+        self.file_path_ctrl = QLineEdit(self.widget1)
         self.file_path_ctrl.setEnabled(False)
         self.file_path_ctrl.setStyleSheet("color: #3CB371;")
         self.file_path_ctrl.setPlaceholderText("选择要处理的文件")
-        self.file_path_choice_button = QPushButton("选择文件", self)
+        self.file_path_choice_button = QPushButton("选择文件", self.widget1)
         self.file_path_choice_button.clicked.connect(self.choice_file_path)
         hbox_path.addWidget(self.file_path_ctrl)
         hbox_path.addWidget(self.file_path_choice_button)
         vbox.addLayout(hbox_path)
 
-        hbox_path = QHBoxLayout(self)
-        self.save_path_ctrl = QLineEdit(self)
+        hbox_path = QHBoxLayout(self.widget1)
+        self.save_path_ctrl = QLineEdit(self.widget1)
         self.save_path_ctrl.setEnabled(False)
         self.save_path_ctrl.setStyleSheet("color: #3CB371;")
         self.save_path_ctrl.setPlaceholderText("文件存放路径")
-        self.save_path_choice_button = QPushButton("选择路径", self)
+        self.save_path_choice_button = QPushButton("选择路径", self.widget1)
         self.save_path_choice_button.clicked.connect(self.choice_save_path)
         hbox_path.addWidget(self.save_path_ctrl)
         hbox_path.addWidget(self.save_path_choice_button)
         vbox.addLayout(hbox_path)
 
-        hbox_log = QHBoxLayout(self)
-        self.log_ctrl = QTextEdit(self)
+        hbox_log = QHBoxLayout(self.widget1)
+        self.log_ctrl = QTextEdit(self.widget1)
         self.log_ctrl.setReadOnly(True)
         self.log_ctrl.setStyleSheet("color: #3CB371;")
         hbox_log.addWidget(self.log_ctrl)
         vbox.addLayout(hbox_log)
+
+        self.addTab(self.widget1, "拆分")
+
+
+        # 提取货号
+        self.widget2 = QWidget(self)
+        vbox2 = QVBoxLayout(self.widget2)
+        self.widget1.setLayout(vbox2)
+
+        hbox_start = QHBoxLayout(self.widget2)
+        self.start_button2 = QPushButton("开始处理", self.widget2)
+        self.start_button2.clicked.connect(self.start_handle2)
+        hbox_start.addWidget(self.start_button2)
+        vbox2.addLayout(hbox_start)
+
+        hbox_path = QHBoxLayout(self.widget2)
+        self.file_path_ctrl2 = QLineEdit(self.widget2)
+        self.file_path_ctrl2.setEnabled(False)
+        self.file_path_ctrl2.setStyleSheet("color: #3CB371;")
+        self.file_path_ctrl2.setPlaceholderText("选择要处理的文件")
+        self.file_path_choice_button2 = QPushButton("选择文件", self.widget2)
+        self.file_path_choice_button2.clicked.connect(self.choice_file_path2)
+        hbox_path.addWidget(self.file_path_ctrl2)
+        hbox_path.addWidget(self.file_path_choice_button2)
+        vbox2.addLayout(hbox_path)
+
+        hbox_path = QHBoxLayout(self.widget2)
+        self.save_path_ctrl2 = QLineEdit(self.widget2)
+        self.save_path_ctrl2.setEnabled(False)
+        self.save_path_ctrl2.setStyleSheet("color: #3CB371;")
+        self.save_path_ctrl2.setPlaceholderText("文件存放路径")
+        self.save_path_choice_button2 = QPushButton("选择路径", self.widget2)
+        self.save_path_choice_button2.clicked.connect(self.choice_save_path2)
+        hbox_path.addWidget(self.save_path_ctrl2)
+        hbox_path.addWidget(self.save_path_choice_button2)
+        vbox2.addLayout(hbox_path)
+
+        hbox_log = QHBoxLayout(self.widget2)
+        self.log_ctrl2 = QTextEdit(self.widget2)
+        self.log_ctrl2.setReadOnly(True)
+        self.log_ctrl2.setStyleSheet("color: #3CB371;")
+        hbox_log.addWidget(self.log_ctrl2)
+        vbox2.addLayout(hbox_log)
+
+        self.addTab(self.widget2, "提取货号")
+
 
     def init_save(self):
         self.name_list = ["file_path_ctrl", "save_path_ctrl"]
@@ -299,6 +348,38 @@ class MainFrame(QWidget):
         folder = QFileDialog.getExistingDirectory(self, '选择保存路径')
         if folder:
             self.save_path_ctrl.setText(folder)
+
+    def start_handle2(self):
+        """开始处理"""
+        self.log_ctrl2.append("开始！")
+        file_path = self.file_path_ctrl2.text()
+        if not os.path.exists(file_path):
+            self.signal_hint_error.emit("要先选择一个有效的文件夹才能开始哦")
+            return
+        save_path = self.save_path_ctrl2.text()
+        if not os.path.exists(save_path):
+            self.signal_hint_error.emit("保存路径不对诶")
+            return
+        value_list = []
+        for file_name in os.listdir(file_path):
+            if file_name.endswith(".jpg") or file_name.endswith(".png"):
+                value_list.append(file_name.split(".")[0])
+        content = "\n".join(value_list)
+        with open(os.path.join(save_path, "output.txt"), "w", encoding="utf-8") as f:
+            f.write(content)
+        self.log_ctrl2.append("搞定！")
+
+    def choice_file_path2(self):
+        """加载url"""
+        folder = QFileDialog.getExistingDirectory(self, '选择保存路径')
+        if folder:
+            self.file_path_ctrl2.setText(folder)
+
+    def choice_save_path2(self):
+        """选择保存路径"""
+        folder = QFileDialog.getExistingDirectory(self, '选择保存路径')
+        if folder:
+            self.save_path_ctrl2.setText(folder)
 
     def hint_error(self, message):
         """弹窗提示错误"""
